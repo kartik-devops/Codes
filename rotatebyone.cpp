76c90f13f2358070163adc12167c3a408f1bdbd6
Kartik Ganotra <kartikeshavg@gmail.com>
2021-07-30 11:27:16 +0530

    Rotatebydno's

---
+++ rotatebyone.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    
+    int n,d;
+    cin>>n>>d;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[(i+n-d)%n];
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n";
+    }
+    return 0;
+}
